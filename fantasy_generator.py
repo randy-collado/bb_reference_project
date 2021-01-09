8aebf32e6b9f6b71ced40da43627d3d86e30174b
Randy Collado <randy64@bu.edu>
2021-01-09 18:36:33 -0400

    Added a writer function to write roster data to individual files(debug tool)

--- fantasy_generator.py
+++ fantasy_generator.py
@@ -1,8 +1,7 @@
-import pandas as pd 
+
 
 def fantasy_file_writer(year, fantasy_site, team):
-    fh = open('roster_{}_{}'.format(year, fantasy_site), 'w')
-    team_df = pd.DataFrame()
+    roster_dict = dict()
     if fantasy_site == "DraftKings":
         roster_dict = {'PG': , 'SG': , 'SF': , 'PF': , 'C': }
     elif fantasy_site == "ESPN":
@@ -13,6 +12,15 @@ def fantasy_file_writer(year, fantasy_site, team):
         import os
         os.remove('roster_{}_{}'.format(year, fantasy_site))
         print("Entered fantasy format currently not supported")
+
+    write_roster(year, fantasy_site, roster_dict)
+
+def write_roster(year, fantasy_site, _dict):
+    with open('roster_{}_{}'.format(year, fantasy_site), 'w') as fh:
+        for k, v in _dict:
+            fh.write('{}: {}'.format(k, v))
+
+            
 
 
  
